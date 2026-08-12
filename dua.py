6f6cbe0871ecd733a72b3a5b897804a1e5270831
shesiliaRPL2TJP <shesiliarpltjp@gmail.com>
2026-08-12 08:11:47 +0700

    Add files via upload

---
+++ dua.py
@@ -0,0 +1,19 @@
+# Program pertamaku
+# Nama: [shesil]
+# Kelas: X RPL
+
+print ("===============================")
+print ("   SELAMAT DATANG DI PYTHON    ")
+print ("===============================")
+print ()
+print ("Program ini dibuat oleh:", "[shesil]")
+print ("Sekolah: SMK TJP Tuban")
+print ("Tahun  :", 2024)
+
+print(10 + 5)
+print(20 - 8)
+print(4 * 7)
+print(15 / 4)
+print(15 // 4)
+print(15 % 4)
+print(2 ** 8)
