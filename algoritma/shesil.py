71d63afd5af86972f495ad4636241ade66063fd6
shesiliaRPL2TJP <shesiliarpltjp@gmail.com>
2026-08-12 08:05:42 +0700

    Delete algoritma directory

--- algoritma/shesil.py
+++
@@ -1,6 +0,0 @@
-# ini adalah komentar - tidak dijalankan oleh python
-# program: salam perkenalan
-
-print("hallo, dunia")            # menampilkan teks
-print("nama saya: shesil")       #setiap print () = satu baris output
-print("versi: 3.x")              #tanda kutip bisa ganda atau tunggal
