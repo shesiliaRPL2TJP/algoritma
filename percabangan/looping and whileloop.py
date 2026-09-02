e0a6d15212dadeec8fd8be14a5d47959558e4031
shesiliaRPL2TJP <shesiliarpltjp@gmail.com>
2026-09-02 12:36:12 +0700

    Add files via upload

---
+++ percabangan/looping and whileloop.py
@@ -0,0 +1,11 @@
+# contoh lengkap penggunaan range ()
+print ("Genap 1-10 :", end=" ")
+for i in renge(0, 11, 2):
+    print (i, end="")
+# input 0 2 4 6 8 10
+
+print ()
+print ("hitung mundur :", end=" ")
+for i in range(5, 0, -1):
+    print (i, end="")
+# input 5 4 3 2 1 
